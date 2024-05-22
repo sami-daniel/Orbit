38c2dd862487e8b19f319b61c3cfc23ab6eef3b4
Sami Daniel <samidaniel125@gmail.com>
2024-05-21 22:19:50 -0300

    docs: Documentando controlador Home

--- Orbit/Controllers/HomeController.cs
+++ Orbit/Controllers/HomeController.cs
@@ -4,9 +4,13 @@ namespace Orbit.Controllers
 {
     public class HomeController : Controller
     {
+        //Route: {url}/Home/Index OR {url}/Home/
         public IActionResult Index()
         {
-            return View();
+            ViewBag.Title = "Bedrock Default Page"; /*Colocando nova propriedade chamada Title<dynamic>
+                                                     dentro da ViewBag (usado para title da pagina de _Layout)*/
+
+            return View(); //~/Views/Home/Index.cshtml
         }
     }
 }
