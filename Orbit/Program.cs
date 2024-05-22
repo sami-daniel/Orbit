fc6c33b19b64c800b729e27a342117a834e648cd
SAMI_DANIEL_SANTOS_SILVA <22201491@DELTAF25.FLORESTA>
2024-05-22 11:33:50 -0300

    refactor: Tirar instruções de nivel superior para facilitar legibilidade

--- Orbit/Program.cs
+++ Orbit/Program.cs
@@ -1,23 +1,33 @@
-var builder = WebApplication.CreateBuilder(args);
-
-//Registrando controllers e views como serviços
-builder.Services.AddControllersWithViews();
-
-var app = builder.Build();
-
-//Habilitando página de erros para desenvolvedor
-if (app.Environment.IsDevelopment())
+namespace Orbit
 {
-    app.UseDeveloperExceptionPage();
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            //Registrando controllers e views como serviços
+            builder.Services.AddControllersWithViews();
+
+            var app = builder.Build();
+
+            //Habilitando página de erros para desenvolvedor
+            if (app.Environment.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            //Habilitando middleware para lidar com requisicoes por arquivos estaticos
+            app.UseStaticFiles();
+
+            //Habilitando roteamento
+            app.UseRouting();
+
+            //Mapeando endpoints para metodos IAction com rota padrão
+            app.MapDefaultControllerRoute();
+
+            app.Run();
+
+        }
+    }
 }
-
-//Habilitando middleware para lidar com requisicoes por arquivos estaticos
-app.UseStaticFiles();
-
-//Habilitando roteamento
-app.UseRouting();
-
-//Mapeando endpoints para metodos IAction com rota padrão
-app.MapDefaultControllerRoute();
-
-app.Run();
