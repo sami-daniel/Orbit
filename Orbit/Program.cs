52f3d83ba4598b2c2af26457b35ae21f2e1d8d84
Sami Daniel <samidaniel125@gmail.com>
2024-05-21 21:48:35 -0300

    feat: Habiitar middlewares padrão para uma aplicação MVC (Arquivos estaticos, roteamento e Mapeamento de rotas padrão para controllers e metodos IAction

--- Orbit/Program.cs
+++ Orbit/Program.cs
@@ -1,6 +1,19 @@
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+//Habilitando página de erros para desenvolvedor
+if (app.Environment.IsDevelopment())
+{
+    app.UseDeveloperExceptionPage();
+}
+
+//Habilitando middleware para lidar com requisicoes por arquivos estaticos
+app.UseStaticFiles();
+
+//Habilitando roteamento
+app.UseRouting();
+
+//Mapeando endpoints para metodos IAction com rota padrão
+app.MapDefaultControllerRoute();
 
 app.Run();
