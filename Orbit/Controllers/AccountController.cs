59795840f63c71bdcadd448de9737a423c8915a1
Sami Daniel <samidaniel125@gmail.com>
2024-05-22 21:24:30 -0300

    feat: Criar controlador account

---
+++ Orbit/Controllers/AccountController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Orbit.Controllers
+{
+    public class AccountController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
