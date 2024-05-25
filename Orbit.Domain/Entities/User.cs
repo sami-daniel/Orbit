187ed144d99036eeef2f188960de50e4f066960e
Sami Daniel <samidaniel125@gmail.com>
2024-05-25 00:41:57 -0300

    fact: Criar modelo de dominio atraves do scaffolding no efcore database-first

---
+++ Orbit.Domain/Entities/User.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Orbit.Domain.Entities;
+
+public partial class User
+{
+    public int UserId { get; set; }
+
+    public string UserName { get; set; } = null!;
+
+    public string UserEmail { get; set; } = null!;
+
+    public DateOnly UserDateOfBirth { get; set; }
+
+    public string UserPassword { get; set; } = null!;
+}
