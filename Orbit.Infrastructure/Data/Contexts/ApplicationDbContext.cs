6585fc4d0122994b5580cdf29ef3a4eb440745bc
Sami Daniel <samidaniel125@gmail.com>
2024-05-25 00:42:24 -0300

    feat: Gerar contexto do banco de dados via scaffolding

---
+++ Orbit.Infrastructure/Data/Contexts/ApplicationDbContext.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+using Orbit.Domain.Entities;
+
+namespace Orbit.Infrastructure.Data.Contexts;
+
+public partial class ApplicationDbContext : DbContext
+{
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<User> Users { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder
+            .UseCollation("utf8mb4_0900_ai_ci")
+            .HasCharSet("utf8mb4");
+
+        modelBuilder.Entity<User>(entity =>
+        {
+            entity.HasKey(e => e.UserId).HasName("PRIMARY");
+
+            entity.ToTable("user");
+
+            entity.Property(e => e.UserId).HasColumnName("user_id");
+            entity.Property(e => e.UserDateOfBirth).HasColumnName("user_date_of_birth");
+            entity.Property(e => e.UserEmail)
+                .HasMaxLength(200)
+                .HasColumnName("user_email");
+            entity.Property(e => e.UserName)
+                .HasMaxLength(100)
+                .HasColumnName("user_name");
+            entity.Property(e => e.UserPassword)
+                .HasMaxLength(200)
+                .HasColumnName("user_password");
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
