ebd0dba8cadaad6912549a1f935f8dc6663432d6
Sami Daniel <samidaniel125@gmail.com>
2024-05-25 00:51:50 -0300

    feat: Criar abstração de repository

---
+++ Orbit.Infrastructure/Repositories/Interfaces/IRepository.cs
@@ -0,0 +1,15 @@
+﻿namespace Orbit.Infrastructure.Repositories.Interfaces
+{
+    public interface IRepository<TEntity> where TEntity : class
+    {
+        Task<TEntity> GetAsync(int id);
+        Task<IEnumerable<TEntity>> GetAllAsync();
+        Task<IEnumerable<TEntity>> Find(Predicate<TEntity> predicate);
+
+        Task<bool> AddAsync(TEntity entity);
+        Task<bool> AddRangeAsync(IEnumerable<TEntity> entities);
+
+        Task<bool> RemoveAsync(TEntity entity);
+        Task<bool> RemoveRangeAsync(IEnumerable<TEntity> entities);
+    }
+}
