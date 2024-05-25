b63fc4827aa3bb78a597a49ae008c3007d13ad3f
Sami Daniel <samidaniel125@gmail.com>
2024-05-25 00:58:58 -0300

    chore/feat: Criar implementação de IRepository

---
+++ Orbit.Infrastructure/Repositories/Implementations/Repository.cs
@@ -0,0 +1,42 @@
+﻿using Orbit.Infrastructure.Repositories.Interfaces;
+
+namespace Orbit.Infrastructure.Repositories.Implementations
+{
+    public class Repository<TEntity> : IRepository<TEntity> where TEntity : class
+    {
+        public Task<bool> AddAsync(TEntity entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> AddRangeAsync(IEnumerable<TEntity> entities)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<TEntity>> Find(Predicate<TEntity> predicate)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<TEntity>> GetAllAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<TEntity> GetAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> RemoveAsync(TEntity entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> RemoveRangeAsync(IEnumerable<TEntity> entities)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
