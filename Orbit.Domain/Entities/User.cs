c101b6661ab4a640c9884f08496976d9748b13bf
Sami Daniel <samidaniel125@gmail.com>
2024-06-16 22:28:07 -0300

    feature: Adicionar ImageByteType (Blob), UserDesc, e UserProfile name (nulos) ao modelo

--- Orbit.Domain/Entities/User.cs
+++ Orbit.Domain/Entities/User.cs
@@ -15,6 +15,12 @@ public partial class User
 
     public string UserPassword { get; set; } = null!;
 
+    public string? UserDescription { get; set; }
+
+    public byte[]? UserImageByteType { get; set; }
+
+    public string? UserProfileName { get; set; }
+
     public virtual ICollection<User> Followers { get; set; } = new List<User>();
 
     public virtual ICollection<User> Users { get; set; } = new List<User>();
