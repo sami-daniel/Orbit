df56fdd9dd8e178271da8c80e02578b2e93b0dbd
Kaio Henrique <m4conh3iroxelbo500.kaio@gmail.com>
2024-06-17 21:57:58 -0300

    SEARCH FEITA!

---
+++ Orbit/Controllers/SearchController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Orbit.Controllers
+{
+    public class SearchController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
