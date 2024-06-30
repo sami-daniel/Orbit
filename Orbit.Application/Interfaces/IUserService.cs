aedad0be31060894ea0f9edfa5e2f35dd5c191b1
Sami Daniel <samidaniel125@gmail.com>
2024-06-29 21:11:14 -0300

    Dois metodos adicionados: FindUserAsync para retornar os usuario com base em um predicado, e GetAllUserAsync para incluir uma propriedade

--- Orbit.Application/Interfaces/IUserService.cs
+++ Orbit.Application/Interfaces/IUserService.cs
@@ -1,5 +1,6 @@
 ﻿using Orbit.Application.Dtos.Requests;
 using Orbit.Application.Dtos.Responses;
+using System.Linq.Expressions;
 
 namespace Orbit.Application.Interfaces
 {
@@ -7,5 +8,7 @@ namespace Orbit.Application.Interfaces
     {
         Task<UserResponse> AddUserAsync(UserAddRequest userAddRequest);
         Task<IEnumerable<UserResponse>> GetAllUsersAsync();
+        Task<IEnumerable<UserResponse>> GetAllUsersAsync(params string[] navProperties);
+        Task<IEnumerable<UserResponse>> FindUsersAsync(Expression<Func<UserResponse, bool>> predicate);
     }
 }
