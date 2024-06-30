85e2bafe8148bb6661511323eefc8224ed344d2b
Sami Daniel <samidaniel125@gmail.com>
2024-06-29 22:42:19 -0300

    uncommenting

--- Orbit/Program.cs
+++ Orbit/Program.cs
@@ -14,19 +14,14 @@ namespace Orbit
         {
             WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
 
-            //Registrando controllers e views como serviços
             _ = builder.Services.AddControllersWithViews();
 
-            //Adicionando contexto de banco de dados como serviço
             _ = builder.Services.AddDbContext<ApplicationDbContext>(opt =>
             opt.UseMySql(connectionString: builder.Configuration.GetConnectionString("OrbitConnection"),
              serverVersion: new MySqlServerVersion(new Version(8, 4, 0))));
 
-
-            //Registrando UserRepository como servico
             _ = builder.Services.AddScoped<IUserRepository, UserRepository>();
 
-            //Registrando UnitOfWork como servico
             _ = builder.Services.AddScoped<IUnitOfWork, UnitOfWork>();
 
             _ = builder.Services.AddScoped<IUserService, UserService>();
@@ -35,7 +30,6 @@ namespace Orbit
 
             WebApplication app = builder.Build();
 
-            //Habilitando página de erros para desenvolvedor
             if (app.Environment.IsDevelopment())
             {
                 _ = app.UseDeveloperExceptionPage();
@@ -49,18 +43,12 @@ namespace Orbit
                 _ = app.UseHsts();
             }
 
-
-            //Habilitando middleware para lidar com requisicoes por arquivos estaticos
             _ = app.UseStaticFiles();
 
-            //Habilitando roteamento
             _ = app.UseRouting();
 
             _ = app.UseSession();
 
-            //Mapeando endpoint padrã como para o metodo Index do Controlador Profile
-            //Não há nescessidade de redirecionar do Index do controlador Home para o Profile,
-            //uma vez que ele nã executa ação alguma
             _ = app.MapControllerRoute(name: "default",
                                        pattern: "{controller=Account}/{action=Index}");
 
