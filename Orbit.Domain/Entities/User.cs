e90c61fee34065e8edfb636d1e48f32e9311275a
Sami Daniel <130937402+sami-daniel@users.noreply.github.com>
2024-07-01 15:06:04 -0300

    Update User.cs
    Adicionar propriedade IsPrivateProfile

--- Orbit.Domain/Entities/User.cs
+++ Orbit.Domain/Entities/User.cs
@@ -7,6 +7,7 @@ public partial class User
     public string UserEmail { get; set; } = null!;
     public string UserPassword { get; set; } = null!;
     public string UserProfileName { get; set; } = null!
+    public bool IsPrivateProfile { get; set; } = null!
     public string? UserDescription { get; set; }
     public byte[]? UserImageByteType { get; set; }
     public virtual ICollection<User> Followers { get; set; } = [];
