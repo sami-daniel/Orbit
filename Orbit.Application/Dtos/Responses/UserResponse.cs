61e8bfb8c3537b0a492945bfa1b098f94835579e
Sami Daniel <130937402+sami-daniel@users.noreply.github.com>
2024-07-01 14:40:15 -0300

    Update UserResponse.cs
    Remover UserDateOfBirth, tornar requerido o UserProfileName,  readaptar ToUserResponseInternal()  para as novas propriedades

--- Orbit.Application/Dtos/Responses/UserResponse.cs
+++ Orbit.Application/Dtos/Responses/UserResponse.cs
@@ -7,13 +7,12 @@ namespace Orbit.Application.Dtos.Responses
         public uint UserId { get; set; }
         public string UserName { get; set; } = null!;
         public string UserEmail { get; set; } = null!;
-        public DateOnly UserDateOfBirth { get; set; }
         public string UserPassword { get; set; } = null!;
+        public string? UserProfileName { get; set; } = null!
         public string? UserDescription { get; set; }
         public byte[]? UserImageByteType { get; set; }
-        public string? UserProfileName { get; set; }
-        public ICollection<UserResponse>? Followers { get; set; }
-        public ICollection<UserResponse>? Users { get; set; }
+        public ICollection<UserResponse> Followers { get; set; }
+        public ICollection<UserResponse> Users { get; set; }
     }
 
     public static class UserExtensions
@@ -32,10 +31,9 @@ namespace Orbit.Application.Dtos.Responses
                     UserId = user.UserId,
                     UserName = user.UserName,
                     UserEmail = user.UserEmail,
-                    UserDateOfBirth = user.UserDateOfBirth,
                     UserPassword = user.UserPassword,
-                    UserDescription = user.UserDescription,
                     UserProfileName = user.UserProfileName,
+                    UserDescription = user.UserDescription,
                     UserImageByteType = user.UserImageByteType,
                     Followers = [],
                     Users = []
@@ -49,10 +47,9 @@ namespace Orbit.Application.Dtos.Responses
                 UserId = user.UserId,
                 UserName = user.UserName,
                 UserEmail = user.UserEmail,
-                UserDateOfBirth = user.UserDateOfBirth,
                 UserPassword = user.UserPassword,
-                UserDescription = user.UserDescription,
                 UserProfileName = user.UserProfileName,
+                UserDescription = user.UserDescription,
                 UserImageByteType = user.UserImageByteType,
                 Followers = user.Followers.Select(u => u.ToUserResponseInternal(new List<uint>(processedUserIds))).ToList(),
                 Users = user.Users.Select(u => u.ToUserResponseInternal(new List<uint>(processedUserIds))).ToList()
