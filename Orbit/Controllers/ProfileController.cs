85dd8d2f34257e832d3c330458ca35e2ae176653
Sami Daniel <samidaniel125@gmail.com>
2024-07-29 16:47:21 -0300

    doidera

--- Orbit/Controllers/ProfileController.cs
+++ Orbit/Controllers/ProfileController.cs
@@ -1,8 +1,11 @@
 ﻿using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Abstractions;
 using Orbit.Application.Dtos.Responses;
 using Orbit.Application.Interfaces;
 using Orbit.Extensions;
+using Orbit.Infrastructure.Data.Contexts;
+using Pomelo.EntityFrameworkCore.MySql.Metadata.Internal;
 using System.Security.Claims;
 
 namespace Orbit.Controllers
@@ -10,11 +13,14 @@ namespace Orbit.Controllers
     [Authorize]
     public class ProfileController : Controller
     {
-        public readonly IUserService _userService;
+        private readonly IUserService _userService;
+        private readonly ApplicationDbContext _context;
 
-        public ProfileController(IUserService userService)
+        public UserResponse UserInSession { get; set; } = null!;
+        public ProfileController(IUserService userService, ApplicationDbContext context)
         {
             _userService = userService;
+            _context = context;
         }
 
         public async Task<IActionResult> Index()
@@ -29,6 +35,7 @@ namespace Orbit.Controllers
                 user = awaiter.Where(u => u.UserName == usr.Value).First();
             }
 
+            UserInSession = user;
             return View(user);
         }
 
@@ -61,6 +68,41 @@ namespace Orbit.Controllers
             var matchProfiles = profiles.Select(p => new { p.UserName, ProfileName = p.UserProfileName, p.UserImageByteType });
 
             return Ok(matchProfiles);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> UploadProfileImage(IFormFile imageFile)
+        {
+            if(imageFile != null && imageFile.Length > 0)
+            {
+                using (var memoryStream = new MemoryStream()) 
+                {
+                    await imageFile.CopyToAsync(memoryStream);
+
+                    var profile = _context.Users.First(u => u.UserName == UserInSession.UserName);
+
+                    profile.UserImageByteType = memoryStream.ToArray();
+
+                    _context.Update(profile);
+                    await _context.SaveChangesAsync();
+                }
+
+                return NoContent();
+            }
+
+            return BadRequest(imageFile);
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetProfileImage(object userID)
+        {
+            var imageEntity = await _context.Users.FindAsync(userID);
+            if (imageEntity == null || imageEntity.UserImageByteType == null)
+            {
+                return NotFound();
+            }
+
+            return File(imageEntity.UserImageByteType, "image/png");
         }
     }
 }
