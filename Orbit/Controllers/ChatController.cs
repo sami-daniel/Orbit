771da14f790b24b41e19d785de9b48c0796f062d
Sami Daniel <samidaniel125@gmail.com>
2024-09-01 18:44:17 -0300

    Add ChatController with Index action method
    Introduced a new `ChatController` class in `ChatController.cs` under the `Orbit.Controllers` namespace. This class inherits from `Controller` and includes a private readonly `_context` field of type `ApplicationDbContext`, initialized via the constructor. The `ChatController` also contains an `Index` action method that returns a view.

---
+++ Orbit/Controllers/ChatController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Orbit.Infrastructure.Data.Contexts;
+
+namespace Orbit.Controllers
+{
+    public class ChatController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+        public ChatController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
