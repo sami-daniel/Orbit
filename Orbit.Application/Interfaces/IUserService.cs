8b5ac8a8d6e959613f62fdffafc71b50e4f5fd27
Sami Daniel <samidaniel125@gmail.com>
2024-09-03 20:09:16 -0300

    Refactor IUserService and remove unused usings
    Removed `using` directives for `Orbit.Application.Dtos.Requests` and `Orbit.Application.Dtos.Responses`. Simplified `IUserService` by removing several methods: `AddUserAsync`, `GetAllUsersAsync`, and `FindUsersAsync` with their overloads. Enclosed `IUserService` within curly braces.

--- Orbit.Application/Interfaces/IUserService.cs
+++ Orbit.Application/Interfaces/IUserService.cs
@@ -1,20 +1,6 @@
-﻿using Orbit.Application.Dtos.Requests;
-using Orbit.Application.Dtos.Responses;
-
-namespace Orbit.Application.Interfaces
+﻿namespace Orbit.Application.Interfaces
 {
     public interface IUserService
     {
-        Task<UserResponse> AddUserAsync(UserAddRequest userAddRequest);
-
-        [Obsolete("Esse metodo retorna uma lista completa de todos os usuários. A iteração sobre essa coleção causará instabilidade e uso excessivo de recursos. Use FindAsync em vez de GetAllUsersAsync")]
-        Task<IEnumerable<UserResponse>> GetAllUsersAsync();
-
-        [Obsolete("Esse metodo retorna uma lista completa de todos os usuários. A iteração sobre essa coleção causará instabilidade e uso excessivo de recursos. Use FindAsync em vez de GetAllUsersAsync()")]
-        Task<IEnumerable<UserResponse>> GetAllUsersAsync(params string[] navProperties);
-
-        Task<IEnumerable<UserResponse>> FindUsersAsync(object conditions);
-
-        Task<IEnumerable<UserResponse>> FindUsersAsync(object conditions, params string[] navProperties);
     }
 }
