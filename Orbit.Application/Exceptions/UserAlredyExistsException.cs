e357be73c799094a5aa44ddf948536d334ed60b8
Sami Daniel <samidaniel125@gmail.com>
2024-09-04 20:54:16 -0300

    Add UserAlredyExistsException class
    Introduce a custom exception class `UserAlredyExistsException` in the `Orbit.Application.Exceptions` namespace. This class includes three constructors: a parameterless constructor, a constructor with a message string, and a constructor with both a message string and an inner exception. Added necessary `using` directives for required namespaces.

---
+++ Orbit.Application/Exceptions/UserAlredyExistsException.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Orbit.Application.Exceptions;
+public class UserAlredyExistsException : Exception
+{
+    public UserAlredyExistsException()
+    {
+    }
+
+    public UserAlredyExistsException(string? message) : base(message)
+    {
+    }
+
+    public UserAlredyExistsException(string? message, Exception? innerException) : base(message, innerException)
+    {
+    }
+}
