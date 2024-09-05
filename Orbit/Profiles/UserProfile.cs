d47383beb0f41098eabbe10fcb977e42db696f6f
Sami Daniel <samidaniel125@gmail.com>
2024-09-05 20:24:01 -0300

    Add UserProfile class with AutoMapper configuration
    Introduced a new UserProfile class within the Orbit.Profiles namespace.
    Configured AutoMapper mappings between UserAddRequest and User entities.
    Ignored specific User properties in the mapping.
    Added custom mapping for IsPrivateProfile property.

---
+++ Orbit/Profiles/UserProfile.cs
@@ -0,0 +1,20 @@
+﻿using AutoMapper;
+using Orbit.Domain.Entities;
+using Orbit.DTOs.Requests;
+
+namespace Orbit.Profiles;
+
+public class UserProfile : Profile
+{
+    public UserProfile()
+    {
+        CreateMap<UserAddRequest, User>()
+                 .ForMember(dest => dest.UserId, opt => opt.Ignore())
+                 .ForMember(dest => dest.UserDescription, opt => opt.Ignore())
+                 .ForMember(dest => dest.UserProfileImageByteType, opt => opt.Ignore())
+                 .ForMember(dest => dest.UserProfileBannerImageByteType, opt => opt.Ignore())
+                 .ForMember(dest => dest.Followers, opt => opt.Ignore())
+                 .ForMember(dest => dest.Users, opt => opt.Ignore())
+                 .ForMember(dest => dest.IsPrivateProfile, opt => opt.MapFrom(src => src.IsPrivateProfile == "on" ? 1UL : 0UL));
+    }
+}
