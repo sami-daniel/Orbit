14c6ee6826471e38bac09a74db7daab747a09025
Sami Daniel <samidaniel125@gmail.com>
2024-09-05 20:31:45 -0300

    Add mapping config in UserProfile for User to UserResponse
    Updated using directives in UserProfile.cs to include Orbit.DTOs.Responses. Added mapping configuration in UserProfile constructor to map User entity to UserResponse DTO. Mapped IsPrivateProfile from 1UL to boolean, preserved references, and mapped Followers and Users properties.

--- Orbit/Profiles/UserProfile.cs
+++ Orbit/Profiles/UserProfile.cs
@@ -1,6 +1,7 @@
 ﻿using AutoMapper;
 using Orbit.Domain.Entities;
 using Orbit.DTOs.Requests;
+using Orbit.DTOs.Responses;
 
 namespace Orbit.Profiles;
 
@@ -16,5 +17,11 @@ public class UserProfile : Profile
                  .ForMember(dest => dest.Followers, opt => opt.Ignore())
                  .ForMember(dest => dest.Users, opt => opt.Ignore())
                  .ForMember(dest => dest.IsPrivateProfile, opt => opt.MapFrom(src => src.IsPrivateProfile == "on" ? 1UL : 0UL));
+
+        CreateMap<User, UserResponse>()
+                .ForMember(dest => dest.IsPrivateProfile, opt => opt.MapFrom(src => src.IsPrivateProfile == 1UL))
+                .PreserveReferences()
+                .ForMember(dest => dest.Followers, opt => opt.MapFrom(src => src.Followers.Select(f => f.Followers)))
+                .ForMember(dest => dest.Users, opt => opt.MapFrom(src => src.Users.Select(u => u.Users)));
     }
 }
