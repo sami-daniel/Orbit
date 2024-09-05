1f7f9b693b709d26b77b23d5a758cc28291c124d
Sami Daniel <samidaniel125@gmail.com>
2024-09-05 20:12:29 -0300

    Add UserAddRequest and UserResponse DTO classes
    Introduce UserAddRequest in Orbit.DTOs.Requests for handling user addition requests with validation annotations. Add UserResponse in Orbit.DTOs.Responses for handling user response data, including nested collections for followers and users.

---
+++ Orbit/DTOs/Responses/UserResponse.cs
@@ -0,0 +1,17 @@
+﻿namespace Orbit.DTOs.Responses
+{
+    public class UserResponse
+    {
+        public uint UserId { get; set; }
+        public string UserName { get; set; } = null!;
+        public string UserEmail { get; set; } = null!;
+        public string UserPassword { get; set; } = null!;
+        public string UserProfileName { get; set; } = null!;
+        public bool IsPrivateProfile { get; set; } = false;
+        public byte[]? UserImageByteType { get; set; }
+        public byte[]? UserBannerByteType { get; set; }
+        public string? UserDescription { get; set; }
+        public ICollection<UserResponse> Followers { get; set; } = [];
+        public ICollection<UserResponse> Users { get; set; } = [];
+    }
+}
