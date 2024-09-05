27dbc6e1f65278a64a09b72089e0bab23858e4bb
Sami Daniel <samidaniel125@gmail.com>
2024-09-05 20:31:59 -0300

    Refactor UserResponse properties
    Removed UserId property from UserResponse class.
    Renamed UserImageByteType to UserProfileImageByteType.
    Renamed UserBannerByteType to UserProfileBannerImageByteType.

--- Orbit/DTOs/Responses/UserResponse.cs
+++ Orbit/DTOs/Responses/UserResponse.cs
@@ -2,13 +2,12 @@
 {
     public class UserResponse
     {
-        public uint UserId { get; set; }
         public string UserName { get; set; } = null!;
         public string UserEmail { get; set; } = null!;
         public string UserProfileName { get; set; } = null!;
         public bool IsPrivateProfile { get; set; } = false;
-        public byte[]? UserImageByteType { get; set; }
-        public byte[]? UserBannerByteType { get; set; }
+        public byte[]? UserProfileImageByteType { get; set; }
+        public byte[]? UserProfileBannerImageByteType { get; set; }
         public string? UserDescription { get; set; }
         public ICollection<UserResponse> Followers { get; set; } = [];
         public ICollection<UserResponse> Users { get; set; } = [];
