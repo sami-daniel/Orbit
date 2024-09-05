0d33cf982152a36b81460ce80933a95ef87a6540
Sami Daniel <samidaniel125@gmail.com>
2024-09-04 21:13:04 -0300

    Add XML docs to exception classes and clean up usings
    Updated InvalidImageException.cs, UserAlredyExistsException.cs, and UserNotFoundException.cs to include XML documentation comments for the classes and their constructors. Removed unnecessary using directives from UserAlredyExistsException.cs.

--- Orbit.Application/Exceptions/UserAlredyExistsException.cs
+++ Orbit.Application/Exceptions/UserAlredyExistsException.cs
@@ -1,22 +1,32 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Runtime.Serialization;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Orbit.Application.Exceptions;
-public class UserAlredyExistsException : Exception
+﻿namespace Orbit.Application.Exceptions
 {
-    public UserAlredyExistsException()
+    /// <summary>
+    /// Exceção lançada quando um usuário com o mesmo identificador já existe.
+    /// </summary>
+    public class UserAlredyExistsException : Exception
     {
-    }
+        /// <summary>
+        /// Inicializa uma nova instância da classe <see cref="UserAlredyExistsException"/>.
+        /// </summary>
+        public UserAlredyExistsException()
+        {
+        }
 
-    public UserAlredyExistsException(string? message) : base(message)
-    {
-    }
+        /// <summary>
+        /// Inicializa uma nova instância da classe <see cref="UserAlredyExistsException"/> com uma mensagem de erro especificada.
+        /// </summary>
+        /// <param name="message">A mensagem que descreve o erro.</param>
+        public UserAlredyExistsException(string? message) : base(message)
+        {
+        }
 
-    public UserAlredyExistsException(string? message, Exception? innerException) : base(message, innerException)
-    {
+        /// <summary>
+        /// Inicializa uma nova instância da classe <see cref="UserAlredyExistsException"/> com uma mensagem de erro especificada e uma referência à exceção interna que é a causa da exceção.
+        /// </summary>
+        /// <param name="message">A mensagem que descreve o erro.</param>
+        /// <param name="innerException">A exceção que é a causa da exceção atual.</param>
+        public UserAlredyExistsException(string? message, Exception? innerException) : base(message, innerException)
+        {
+        }
     }
 }
