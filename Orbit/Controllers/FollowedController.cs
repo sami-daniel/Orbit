d3c5ebf9ebe1eb52e991563a8dc45109918f8a4a
Sami Daniel <samidaniel125@gmail.com>
2024-09-15 20:17:44 -0300

    Add Followed and Follower controllers and update views
    - Updated `AccountController.cs` to change the route for `CreateUser` action to an absolute path.
    - Added `FollowedController.cs` to handle displaying followed users using dependency injection and session data.
    - Added `FollowerController.cs` to handle displaying followers using dependency injection and session data.
    - Updated `Index.cshtml` to display followed users and followers, iterating over the model to show each user's details in a `div` element.

---
+++ Orbit/Controllers/FollowedController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Orbit.Application.Interfaces;
+using Orbit.Domain.Entities;
+using Orbit.DTOs.Responses;
+using Orbit.Extensions;
+
+namespace Orbit.Controllers;
+public class FollowedController : Controller
+{
+    private readonly IUserService _userService;
+
+    public FollowedController(IUserService userService)
+    {
+        _userService = userService;
+    }
+
+    public async Task<IActionResult> Index()
+    {
+        var user = HttpContext.Session.GetObject<UserResponse>("User")!.UserName;
+        var users = await _userService.GetUserByIdentifierAsync(user);
+
+        var followed = users!.Users;
+        return View(followed);
+    }
+}
