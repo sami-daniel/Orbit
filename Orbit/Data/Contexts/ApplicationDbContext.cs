efd1187ccc2bd0a2ed6bf3421fe193ddd55ed5b8
= <=>
2024-11-18 12:02:00 -0300

    Supress warning CS8618

--- Orbit/Data/Contexts/ApplicationDbContext.cs
+++ Orbit/Data/Contexts/ApplicationDbContext.cs
@@ -5,6 +5,7 @@ namespace Orbit.Data.Contexts;
 
 public partial class ApplicationDbContext : DbContext
 {
+#pragma warning disable CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider adding the 'required' modifier or declaring as nullable.
     public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
         : base(options)
     {
