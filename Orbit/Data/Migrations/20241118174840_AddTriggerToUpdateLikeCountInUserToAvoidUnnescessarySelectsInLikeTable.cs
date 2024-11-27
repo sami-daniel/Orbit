f689abea75e974b5cbc7aa8ef4dc0928c9dd06f9
Sami Daniel <sami-daniel-dev@proton.me>
2024-11-27 11:01:15 -0300

    Undoing

--- Orbit/Data/Migrations/20241118174840_AddTriggerToUpdateLikeCountInUserToAvoidUnnescessarySelectsInLikeTable.cs
+++ Orbit/Data/Migrations/20241118174840_AddTriggerToUpdateLikeCountInUserToAvoidUnnescessarySelectsInLikeTable.cs
@@ -14,8 +14,8 @@ namespace Orbit.Data.Migrations
             CREATE TRIGGER `post_like_count_update` AFTER INSERT ON `likes`
             FOR EACH ROW
             BEGIN
-                UPDATE users
-                SET user_like_count = user_like_count + 1
+                UPDATE post
+                SET post_likes = post_likes + 1
                 WHERE user_id = NEW.user_id;
             END;
             """);
