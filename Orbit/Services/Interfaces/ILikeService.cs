3ddf90434fe70636910cf570a856254b88cea56d
Sami Daniel <sami-daniel-dev@proton.me>
2024-11-28 12:55:44 -0300

    Fixing

--- Orbit/Services/Interfaces/ILikeService.cs
+++ Orbit/Services/Interfaces/ILikeService.cs
@@ -5,7 +5,6 @@ namespace Orbit.Services.Interfaces;
 public interface ILikeService
 {
     /// <summary>
-<<<<<<< HEAD
     /// Likes a post.
     /// </summary>
     /// <param name="postID">The ID of the post.</param>
@@ -28,27 +27,3 @@ public interface ILikeService
     /// <returns>A task representing the asynchronous operation. The result contains a list of likes from the user.</returns>
     public Task<IEnumerable<Like>> GetLikesFromUser(string username);
 }
-=======
-    /// Curte um post.
-    /// </summary>
-    /// <param name="postID">ID do post.</param>
-    /// <param name="username">Nome de usuário.</param>
-    /// <returns>Uma tarefa que representa a operação assincrona.</returns>
-    public Task LikePost(uint postID, string username);
-
-    /// <summary>
-    /// Descurte um post.
-    /// </summary>
-    /// <param name="postID">ID do post.</param>
-    /// <param name="username">Nome de usuário.</param>
-    /// <returns>Uma tarefa que representa a operação assincrona.</returns>
-    public Task UnlikePost(uint postID, string username);
-
-    /// <summary>
-    /// Obtém as curtidas de um usuário em um post.
-    /// </summary>
-    /// <param name="username">Nome de usuário.</param>
-    /// <returns>Uma tarefa que representa a operação assincrona.</returns>
-    public Task<IEnumerable<Like>> GetLikesFromUser(string username);
-}
->>>>>>> 35189761d3d1ce18be8ee88be049a8f9dcaf53a6
--- Orbit/Services/Interfaces/IUserPreferenceService.cs
+++ Orbit/Services/Interfaces/IUserPreferenceService.cs
@@ -3,7 +3,6 @@ namespace Orbit.Services.Interfaces;
 public interface IUserPreferenceService
 {
     /// <summary>
-<<<<<<< HEAD
     /// Updates the user's preferences.
     /// </summary>
     /// <param name="userName">The name of the user whose preferences will be updated.</param>
@@ -11,11 +10,3 @@ public interface IUserPreferenceService
     /// <returns>A task representing the asynchronous operation.</returns>
     Task UpdateUserPreferenceAsync(string userName, uint postID);
 }
-=======
-    /// Atualiza as preferências do usuário.
-    /// </summary>
-    /// <param name="userName"></param>
-    /// <returns></returns>
-    Task UpdateUserPreferenceAsync(string userName, uint postID);
-}
->>>>>>> 35189761d3d1ce18be8ee88be049a8f9dcaf53a6
